29c430d75ac2fa50d6d2672549af97806c45d715
xulkif <zulkifazher2@gmail.com>
2023-11-04 05:04:21 -0700

    This code is modified because in the privious push i did not consist m codes on the pused class by mistake

--- Student.java
+++ Student.java
@@ -1,2 +1,23 @@
-public class Student {
-}
+ public class Student extends Person {
+
+        private String classroom;
+
+
+        public Student(String name, int age, boolean parent_permission, String classroom) {
+
+            super(name, age, parent_permission);
+
+            this.classroom = classroom;
+
+        }
+
+
+        public String getClassroom() {
+            System.out.println("jiop");
+            return classroom;
+
+        }
+
+    }
+
+
